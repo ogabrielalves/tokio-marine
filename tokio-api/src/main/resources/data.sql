b793440565e72c406c3d9662744259f7e1b7884c
ogabrielalves <gabrielalvessilva02@outlook.com>
2025-09-03 16:40:16 -0300

    feat: add SQL file of transfers for database initialization

---
+++ tokio-api/src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('12345678', '87654321', 100.00, 5.50, '2025-09-03', '2025-09-03');
+
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('11112222', '33334444', 200.00, 12.00, '2025-09-08', '2025-09-03');
+
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('22223333', '44445555', 500.00, 41.00, '2025-09-18', '2025-09-03');
+
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('33334444', '55556666', 300.00, 20.70, '2025-09-28', '2025-09-03');
+
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('44445555', '66667777', 400.00, 18.80, '2025-10-08', '2025-09-03');
+
+INSERT INTO transfers (source_account, destination_account, amount, fee, transfer_date, scheduling_date)
+VALUES ('55556666', '77778888', 1000.00, 17.00, '2025-10-18', '2025-09-03');
